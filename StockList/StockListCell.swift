71c4ebe2977cf498e8ea194c9c29c18a351a61d9
torinunna <torinunna9588@gmail.com>
2023-05-06 02:39:26 +0900

    add collectionView to StockListVC with StockListCell

---
+++ StockList/StockListCell.swift
@@ -0,0 +1,16 @@
+//
+//  StockListCell.swift
+//  StockList
+//
+//  Created by YUJIN KWON on 2023/05/05.
+//
+
+import UIKit
+
+class StockListCell: UICollectionViewCell {
+    
+    static let identifier = "StockListCell"
+    
+    
+    
+}
--- StockList/StockListViewController.swift
+++ StockList/StockListViewController.swift
@@ -9,10 +9,31 @@ import UIKit
 
 class StockListViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
-   
+    
+}
+
+extension StockListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: StockListCell.identifier, for: indexPath) as? StockListCell else { return UICollectionViewCell() }
+        return cell
+    }
+    
+}
+
+extension StockListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 80.0)
+    }
 }
